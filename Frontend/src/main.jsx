fbbebacf9fb7a235b0c13da9816b09c77226bd0e
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-12-11 15:55:56 +0530

    firebase Setup->import firebase app in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store,persistor } from "./redux/store";
+import { store, persistor } from "./redux/store";
 import "react-toastify/dist/ReactToastify.css";
 import { PersistGate } from "redux-persist/integration/react";
+import { app } from "./firebase.config";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider store={store} app={app}>
     <PersistGate loading={"loading"} persistor={persistor}>
       <App />
     </PersistGate>
