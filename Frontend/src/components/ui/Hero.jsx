d122758171e2911e8e504b8766e2dadb0a331420
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-12-28 00:27:53 +0530

    fix: overflow and margin

--- Frontend/src/components/ui/Hero.jsx
+++ Frontend/src/components/ui/Hero.jsx
@@ -1,12 +1,21 @@
 import { hero } from "../../assets/index";
+
 const HeroSection = () => {
   return (
-    <section className="max-w-6xl mx-auto py-48">
-      <div className="container mx-auto px-4">
+    <section className="relative py-44 -mt-28">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+      </div>
+      <div className="container mx-auto px-4 relative z-10">
         <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
-          <div className="space-y-6">
-            <p className=" font-semibold text-blue-700">Welcome to,</p>
-            <h1 className="text-4xl font-bold capitalize">Kumar Fertilizer</h1>
+          <div className="space-y-6 max-w-[70%] mx-auto">
+            <span>
+              <p className="font-semibold text-blue-700">Welcome to,</p>
+              <h1 className="text-4xl font-bold capitalize">
+                Kumar Fertilizer
+              </h1>
+            </span>
+
             <p className="text-gray-700">
               As India's premier online agri shop, we provide everything you
               need for successful farming from high-quality seeds, fertilizers,
@@ -19,7 +28,7 @@ const HeroSection = () => {
             </p>
           </div>
           <div className="flex justify-center">
-            <div className="relative">
+            <div className="relative mt-6">
               <img
                 src={hero}
                 alt="hero"
